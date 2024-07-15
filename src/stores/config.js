b24e207159927b46ab1600fdad515c7e3f5929e3
Matthew Miller <thisismattmiller@gmail.com>
2024-07-15 13:47:55 -0400

    test dvd record

--- src/stores/config.js
+++ src/stores/config.js
@@ -189,10 +189,14 @@ export const useConfigStore = defineStore('config', {
     {lccn:'2024591512',label:"The Berkshires, Massachusetts discovery map 2022-2023", idUrl:'https://id.loc.gov/resources/instances/23486403.html', profile:'Cartographic',profileId:'lc:RT:bf2:Cartographic:Instance'},
     {lccn:'2016627557',label:"Wallflower", idUrl:'https://id.loc.gov/resources/instances/2016627557.html', profile:'Audio CD',profileId:'lc:RT:bf2:SoundRecording:Instance'},
 
+    {lccn:'2023602524',label:"Bones and all", idUrl:'https://id.loc.gov/resources/instances/23150570.html', profile:'Moving Image: BluRay DVD',profileId:'lc:RT:bf2:MIBluRayDVD:Instance'},
+
+    
 
     
 
   ],
+
 
 
   lookupConfig: {
