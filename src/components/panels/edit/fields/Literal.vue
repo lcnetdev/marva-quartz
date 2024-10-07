35e4af5aa8b6b45b202add38d299b85d3cd64230
Franz Osorio <fosorio@loc.gov>
2024-10-07 09:55:03 -0400

    Don't add dropdown to abbriviated mode

--- src/components/panels/edit/fields/Literal.vue
+++ src/components/panels/edit/fields/Literal.vue
@@ -49,13 +49,7 @@
               <div class="bfcode-display-mode-holder">
                 <div class="bfcode-display-mode-holder-label" :title="structure.propertyLabel">{{profileStore.returnBfCodeLabel(structure)}}</div>
                 <div class="bfcode-display-mode-holder-value">
-                  <template v-if="structure.propertyURI == 'http://id.loc.gov/ontologies/bflc/nonSortNum'">
-                    <select id="nonSort-selection" @change="valueChanged" :ref="'input_' + lValue['@guid']" :data-guid="lValue['@guid']" style="margin-top: .5em;">
-                      <option v-for="(n, opt) in 10" :value="opt" :selected="opt == lValue['value']">{{ opt }}</option>
-                    </select>
-                  </template>
-                  <template v-else>
-                    <textarea
+                  <textarea
                     :class="['literal-textarea', 'can-select',{'bfcode-textarea': preferenceStore.returnValue('--b-edit-main-splitpane-edit-shortcode-display-mode')}]"
                     v-model="lValue.value"
                     v-on:keydown.enter.prevent="submitField"
@@ -68,9 +62,6 @@
                     :data-guid="lValue['@guid']"
                     :disabled="readOnly"
                     ></textarea>
-                  </template>
-
-
                 </div>
               </div>
 
