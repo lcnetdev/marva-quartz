ead68387cb0e84ee77f40300c39b01b863c704d2
f-osorio <fosorio@loc.gov>
2024-10-11 10:19:19 -0400

    Fix contributor emptying out when switching type
    This seems like a naive way of doing things.
    
    The issue was that the carry-over data was being built and stored,
    but when the time came to insert it into the new component there was a
    failure to match on the properties.
    
    The creator it was trying to match on `sameAs` because this is the
    property for the `nextRef` but it saves the carry-over value on the
    `bf:agent` property.
    
    The fix here is to add the `propertyPath` uris to the
    `possibleProperties` to ensure that they are available for the match.

--- src/stores/profile.js
+++ src/stores/profile.js
@@ -926,6 +926,7 @@ export const useProfileStore = defineStore('profile', {
 
         // if there are properties in the old template that are not in the new one then we need to remove them from the userValue
         let possibleProperties = nextRef.propertyTemplates.map((p) => {return p.propertyURI})
+		possibleProperties = possibleProperties.concat(propertyPath.map((p) => {return p.propertyURI}))
 
         if (!pt.refTemplateUserValue){
             pt.refTemplateUserValue = {}
