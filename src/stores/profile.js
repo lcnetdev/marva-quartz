51916eacf80c7cecad5d0b15b9f2ba4f59798bc3
f-osorio <fosorio@loc.gov>
2024-10-17 10:32:59 -0400

    Add bibId to title on load

--- src/stores/profile.js
+++ src/stores/profile.js
@@ -2407,10 +2407,10 @@ export const useProfileStore = defineStore('profile', {
           let env = config.returnUrls.env
 
           // populate the title
-          if (type=='Instance'){
-            let bibId =  this.activeProfile.rt[rt].URI.split("/")[this.activeProfile.rt[rt].URI.split('/').length - 1]
-            document.title = `Marva | ${bibId}`;
-          }
+          // if (type=='Instance'){
+            // let bibId =  this.activeProfile.rt[rt].URI.split("/")[this.activeProfile.rt[rt].URI.split('/').length - 1]
+            // document.title = `Marva | ${bibId}`;
+          // }
 
           pubResuts.resourceLinks.push({
             'type':type,
@@ -2420,10 +2420,7 @@ export const useProfileStore = defineStore('profile', {
         }
       }
 
-
-
       return pubResuts
-
     },
 
 
