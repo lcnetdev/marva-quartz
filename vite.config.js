ead7c59f8b53eca731766d023e54e995988735d2
jim hahn <jimfhahn@gmail.com>
2024-12-02 13:52:25 -0600

    configure for Penn IP ranges (quartz.bibframe.app)

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vue from "@vitejs/plugin-vue";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/bfe2/quartz/',
+  base: '/',
   // build: {
   //   sourcemap: true,
   //   minify: false,
