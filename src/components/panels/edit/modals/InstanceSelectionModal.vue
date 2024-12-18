9dedfbbbc78fb1ed9d9adb74914960b751df0fb6
f-osorio <fosorio@loc.gov>
2024-12-18 14:55:49 -0500

    More styling

--- src/components/panels/edit/modals/InstanceSelectionModal.vue
+++ src/components/panels/edit/modals/InstanceSelectionModal.vue
@@ -6,14 +6,14 @@
         display-directive="show"
         :hide-overlay="true"
         :overlay-transition="'vfm-fade'"
+        :contentClass="instance-selection-modal-container"
       >
       <VueDragResize
             :is-active="true"
             :w="400"
-            :h="500"
+            :h="250"
             :x="200"
             :y="50"
-            :class="instance-selection-modal"
             @resizing="dragResize"
             @dragging="dragResize"
             :sticks="['br']"
@@ -23,18 +23,18 @@
 
             <div class="instance-selection-modal" ref="instanceSelection">
                 <div>
-                    <h3 style="width: 80%; float: left;">Select Location To Send To</h3>
+                    <h3 style="width: 80%; float: left; padding: 5px;">Select Location To Send To</h3>
                     <div class="close-button">
                         <button @click="closeModal()">Close</button>
                     </div>
                 </div>
                 <div class="toggle-btn-grp cssonly">
                     <div>
-                        <input type="radio" id="all" value="all" class="instance-selection-radio" v-model="selectedInstance" name="insetance-selection" @click="setInstance($event, 'all')" />
+                        <input type="radio" id="all" value="all" class="instance-selection-radio" v-model="selectedInstance" name="instance-selection" @click="setInstance($event, 'all')" />
                         <label onclick="" for="all" class="dewey-toggle-btn">All</label>
                     </div>
                     <div v-for="(value, key, idx) in instances">
-                        <input type="radio" :id="key" :value="key" class="instance-selection-radio" v-model="selectedInstance" name="insetance-selection" @click="setInstance($event, idx)" />
+                        <input type="radio" :id="key" :value="key" class="instance-selection-radio" v-model="selectedInstance" name="instance-selection" @click="setInstance($event, idx)" />
                         <label onclick="" :for="key" class="dewey-toggle-btn">{{updateLabel(key)}}</label>
                     </div>
                 </div>
@@ -43,19 +43,24 @@
     </VueFinalModal>
   </template>
 
-<style>
+<style scoped>
+
     .instance-selection-modal {
+        height: inherit;
         background-color: whitesmoke;
+        border: 1px solid black;
+        overflow-y: scroll;
     }
 
     .close-button {
         float: right;
         width: 20%;
+        padding: 5px;
     }
 
     .toggle-btn-grp.cssonly{
         width: 100%;
-        height: 30px;
+        /* height: inherit; */
         line-height: 30px;
 
         display: grid;
@@ -63,8 +68,6 @@
         flex-wrap: wrap
     }
     .toggle-btn-grp.cssonly div {
-        display: initial;
-        position: relative;
         margin: 5px 2px;
     }
 
