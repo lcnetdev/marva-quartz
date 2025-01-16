92bdc948c7c1eb220e0ebfca65c1c5bccef1c904
Matthew Miller <thisismattmiller@gmail.com>
2025-01-16 14:34:16 -0500

    component library interface

--- src/components/panels/sidebar_property/Properties.vue
+++ src/components/panels/sidebar_property/Properties.vue
@@ -15,7 +15,10 @@
           //'http://id.loc.gov/ontologies/bibframe/hasInstance',
           //'http://id.loc.gov/ontologies/bibframe/instanceOf',
           'http://id.loc.gov/ontologies/bibframe/hasItem'
-        ]
+        ],
+
+        activeComponentLibrary: null,
+        clDebounce:false,
 
       }
     },
@@ -33,7 +36,7 @@
       // gives access to this.counterStore and this.userStore
       ...mapStores(useProfileStore,usePreferenceStore),
       // // gives read access to this.count and this.double
-      ...mapState(useProfileStore, ['profilesLoaded','activeProfile', 'dataChanged','rtLookup', 'activeComponent']),
+      ...mapState(useProfileStore, ['profilesLoaded','activeProfile', 'dataChanged','rtLookup', 'activeComponent','returnComponentLibrary']),
       ...mapState(usePreferenceStore, ['styleDefault', 'isEmptyComponent']),
 
       ...mapWritableState(useProfileStore, ['activeComponent']),
@@ -43,6 +46,108 @@
 
 
     methods: {
+
+
+      configComponentLibrary(clId){
+        if (this.activeComponentLibrary == clId){
+          this.activeComponentLibrary = null
+          return
+        }
+        this.activeComponentLibrary = clId
+      },
+
+      configComponentLibraryAssignGroup(event,clId){
+        this.profileStore.changeGroupComponentLibrary(clId,event.target.value)
+
+        
+
+      },
+
+      delComponentLibrary(event,clId){
+
+
+        let newId = this.profileStore.delComponentLibrary(clId)
+
+
+      },
+      renameComponentLibrary(event,clId,oldLabel){
+
+        let newName = prompt("New Name?",oldLabel )
+        if (!newName){return false}
+        let newId = this.profileStore.renameComponentLibrary(clId,newName)
+
+
+      },
+
+      
+      
+      addComponentLibrary(event,clId,supressPropmpt){
+        
+        if (event){
+          if (this.clDebounce){return false}
+          event.preventDefault()
+          this.clDebounce = true
+          // don't allow multiple clicks fast
+          window.setTimeout(()=>{this.clDebounce = false},100)
+
+        }
+
+
+      
+        if (this.preferenceStore.returnValue('--b-edit-main-splitpane-properties-component-library-prompt-to-add') == true){
+          if (!supressPropmpt){
+            if (!confirm('Add Component From Library?')) {
+              return 'canceled'
+            }
+          }
+        }
+
+        let newId = this.profileStore.addFromComponentLibrary(clId)
+
+        this.activeComponent = this.activeProfile.rt[newId[0]].pt[newId[1]]
+
+        // for (let rt in this.activeProfile.rt){
+        //   for (let pt in this.activeProfile.rt[rt].pt){
+        //     if (this.activeProfile.rt[rt].pt[pt].id == newId){
+              
+        //     }
+        //   }
+        // }
+
+
+        return false
+
+      },
+
+
+      addComponentLibraryGroup(profile,group){
+        console.log(profile,group)
+        console.log(this.returnComponentLibrary)
+        let sentFirstComponentOfGroup = false
+        let supressPrompt = false
+        for (let groups of this.returnComponentLibrary){
+          if (groups.profileId == profile){
+            if (groups.groups[group]){
+              for (let component of groups.groups[group]){
+                console.log(component)
+
+                let r = this.addComponentLibrary(null,component.id,supressPrompt)
+                // if the first one returns canceled then stop, otherwise supress the prompt from here on
+                if (r == 'canceled'){ return false}else{supressPrompt=true}
+                
+              }
+            }
+          }
+        }
+        // for (let group of returnComponentLibrary){
+
+
+        // }
+
+
+
+      },
+
 
       returnSubjectHeadingLabel(component){
 
@@ -127,10 +232,7 @@
 </script>
 
 <template>
-
   <template  v-if="preferenceStore.returnValue('--b-edit-main-splitpane-properties-accordion') == true">
-
-
     <AccordionList  :open-multiple-items="false">
 
       <template v-for="profileName in activeProfile.rtOrder" :key="profileName">
@@ -337,7 +439,94 @@
 
 
 
+  <template v-if="preferenceStore.returnValue('--b-edit-main-splitpane-properties-component-library') == true">
 
+    <AccordionList  :open-multiple-items="true">
+
+      <template v-for="clProfile in returnComponentLibrary" :key="clProfile">
+
+        <AccordionItem style="color: white;" :id="'accordion_'+clProfile.label" default-closed>
+          <template #summary>
+            <div> <span class="material-icons" style="font-size: 18px;padding-left: 2px;">library_add</span> <span style="vertical-align: text-bottom;" class="sidebar-header-text">Library: {{ clProfile.label }}</span></div>          
+          </template>
+          <ul class="sidebar-property-ul" role="list">
+            <template v-for="group in clProfile.groups" >
+
+                <template v-if="group.length>1">
+                  <li class="component-librart-group-line"></li>
+                </template>
+
+                <template v-for="component in group">
+                  <li class="sidebar-property-li sidebar-property-li-cl ">
+
+                  
+
+                  <button :class="{'material-icons' : true, 'component-library-settings-button': true, 'component-library-settings-button-invert': (activeComponentLibrary == component.id)  }" @click="configComponentLibrary(component.id)">settings_applications</button>
+
+                  
+
+                  <div class="component-library-item-container sidebar-property-li-empty" @click="addComponentLibrary($event,component.id)" >
+                    <a href="#" @click="addComponentLibrary($event,component.id)">{{ component.label }}</a>
+                  </div>
+                    <template v-if="activeComponentLibrary == component.id">
+                      <div class="component-library-settings">
+                        
+                        
+                        <button class="material-icons simptip-position-right" data-tooltip="DELETE" @click="delComponentLibrary($event,component.id)">delete_forever</button>
+                        <button class="material-icons simptip-position-right" data-tooltip="RENAME" @click="renameComponentLibrary($event,component.id,component.label)">new_label</button>
+                        <select @change="configComponentLibraryAssignGroup($event,component.id)">
+                          <option value="" :selected="(component.groupId===null)">No Group</option>
+                          <option value="A" :selected="(component.groupId==='A')">Group A</option>
+                          <option value="B" :selected="(component.groupId==='B')">Group B</option>
+                          <option value="C" :selected="(component.groupId==='C')">Group C</option>
+                          <option value="D" :selected="(component.groupId==='D')">Group D</option>
+                          <option value="E" :selected="(component.groupId==='E')">Group E</option>
+                          <option value="F" :selected="(component.groupId==='F')">Group F</option>
+                          <option value="G" :selected="(component.groupId==='G')">Group G</option>
+                          <option value="H" :selected="(component.groupId==='H')">Group H</option>
+                          <option value="I" :selected="(component.groupId==='I')">Group I</option>
+                          <option value="J" :selected="(component.groupId==='J')">Group J</option>
+                          <option value="K" :selected="(component.groupId==='K')">Group K</option>
+                          <option value="L" :selected="(component.groupId==='L')">Group L</option>
+                          <option value="M" :selected="(component.groupId==='M')">Group M</option>
+                          <option value="N" :selected="(component.groupId==='N')">Group N</option>
+                          <option value="O" :selected="(component.groupId==='O')">Group O</option>
+                          <option value="P" :selected="(component.groupId==='P')">Group P</option>
+                          <option value="Q" :selected="(component.groupId==='Q')">Group Q</option>
+                          <option value="R" :selected="(component.groupId==='R')">Group R</option>
+                          <option value="S" :selected="(component.groupId==='S')">Group S</option>
+                          <option value="T" :selected="(component.groupId==='T')">Group T</option>
+                          <option value="U" :selected="(component.groupId==='U')">Group U</option>
+                          <option value="V" :selected="(component.groupId==='V')">Group V</option>
+                          <option value="W" :selected="(component.groupId==='W')">Group W</option>
+                          <option value="X" :selected="(component.groupId==='X')">Group X</option>
+                          <option value="Y" :selected="(component.groupId==='Y')">Group Y</option>
+                          <option value="Z" :selected="(component.groupId==='Z')">Group Z</option>                     
+                        </select>
+                        
+
+                      </div>
+                    </template>
+                  </li>
+                </template>
+
+              <template v-if="group.length>1">
+                <button class="component-librart-group-button" @click="addComponentLibraryGroup(group[0].structure.parentId, group[0].groupId)"><span class="material-icons">arrow_upward</span>Add Group {{ group[0].groupId }} <span class="material-icons">arrow_upward</span></button>
+              </template>
+
+
+
+            </template>
+
+          </ul>
+
+        </AccordionItem>
+
+      </template>
+    </AccordionList>
+  </template>
+
+  
 
 
 
@@ -429,12 +618,85 @@
   display: flex;
 }
 
+.component-library-item-container{
+    width: 100%;
+
+    height: v-bind("preferenceStore.returnValue('--n-edit-main-splitpane-properties-font-size', true) + 0.25  + 'em'");
+    max-height: v-bind("preferenceStore.returnValue('--n-edit-main-splitpane-properties-font-size', true) + 0.25  + 'em'");
+    overflow: hidden;
+    cursor: pointer;
+
+
+}
+.component-library-item-container a{
+  color: inherit !important;
+  text-decoration:none;
+}
+
+.component-librart-group-line{
+  border-bottom: solid 1px #6e6e6e;
+  height: 1px;
+}
+.component-librart-group-button{
+  width: 100%;
+  height: 16px;
+  font-size: 12px;
+
+  padding: 0;
+}
+.component-librart-group-button span{
+  font-size: 12px;
+}
+
+.sidebar-property-li-cl{
+  padding-left: 0 !important;
+  position: relative;
+}
+
+.sidebar-property-li-cl .component-library-settings-button{
+  position: absolute;
+    right: -5px;
+    font-size: 20px;
+    border: none;
+    padding: 0;
+    background-color: white;
+    margin: 0;
+    border-radius: 5px;
+    display: none;
+    z-index: 1000;
+}
+
+.component-library-settings-button-invert{
+  filter: invert(100%);
+}
+
+
+.sidebar-property-li-cl:hover .component-library-settings-button{
+  display: inline-block;
+}
+.component-library-settings{
+    border: solid 0.5px v-bind("preferenceStore.returnValue('--c-edit-main-splitpane-properties-font-color')");
+    border-radius: 3px;
+    margin-left: 12px;
+    padding: 2px;
+
+}
+
+.component-library-settings button{
+  font-size: 20px;
+}
+.component-library-settings select{
+  height: 26px;
+  vertical-align: top;
+  
+
+}
+
 
 .sidebar-header-text{
   font-size: v-bind("preferenceStore.returnValue('--n-edit-main-splitpane-properties-font-size', true) + 0.25  + 'em'");
   font-family: v-bind("preferenceStore.returnValue('--c-edit-main-splitpane-properties-font-family')");
   color: v-bind("preferenceStore.returnValue('--c-edit-main-splitpane-properties-font-color')") !important;
-
 }
 
 .sidebar-property-ul{
