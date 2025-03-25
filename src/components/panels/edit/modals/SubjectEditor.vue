55b41e472267634a37dfbca650e94373a2e88709
f-osorio <fosorio@loc.gov>
2025-03-25 08:21:34 -0400

    clean console statements

--- src/components/panels/edit/modals/SubjectEditor.vue
+++ src/components/panels/edit/modals/SubjectEditor.vue
@@ -1088,7 +1088,6 @@ methods: {
         for (let i in this.componetLookup){
           for (let j in this.componetLookup[i]) {
             targetType = this.componetLookup[i][j].type
-            console.info("targetType: ", targetType)
 
             if (this.componetLookup[i][j].label.includes("--")){
               target = this.componetLookup[i][j].label.replaceAll("--", "‑‑")
@@ -1164,7 +1163,6 @@ methods: {
       if (this.typeLookup[id+offset]){
         type = this.typeLookup[id+offset]
       }
-      console.info("building -- type: ", type)
 
       this.components.push({
         label: ss,
@@ -1189,7 +1187,6 @@ methods: {
     //make sure the searchString matches the components
     this.subjectString = this.components.map((component) => component.label).join("--")
 
-    console.info("final components: ", JSON.parse(JSON.stringify(this.components)))
   },
 
   /**
@@ -2276,7 +2273,6 @@ methods: {
   },
 
   updateAvctiveTypeSelected: function(){
-    console.info("updateAvctiveTypeSelected: ", JSON.parse(JSON.stringify(this.activeComponent)))
     //set them all false
     for (let k in this.activeTypes){
       this.activeTypes[k].selected=false
@@ -2313,21 +2309,10 @@ methods: {
   },
 
   setTypeClick: function(event,type){
-    console.info("setTypeClick: ", type)
     this.activeComponent.type = type
     this.typeLookup[this.activeComponentIndex] = type
-
-    // console.info("this.componetLookup: ", JSON.parse(JSON.stringify(this.componetLookup)))
-    // console.info("this.componetLookup: ", JSON.parse(JSON.stringify(this.componetLookup[this.activeComponentIndex])))
-
-    // this.componetLookup[this.activeComponentIndex].extra.type = type
-
-    console.info("this.typeLookup: ", JSON.parse(JSON.stringify(this.typeLookup)))
-    console.info("this.activeComponent: ", JSON.parse(JSON.stringify(this.activeComponent)))
-
     this.subjectStringChanged()
     this.$refs.subjectInput.focus()
-    console.info("this.activeComponent: ", JSON.parse(JSON.stringify(this.activeComponent)))
   },
 
   renderHintBoxes: function(){
@@ -2377,7 +2362,6 @@ methods: {
   },
 
   subjectStringChanged: async function(event){
-    console.info("subjectStringChanged: ", JSON.parse(JSON.stringify(this.activeComponent)))
     this.subjectString=this.subjectString.replace("—", "--")
     this.validateOkayToAdd()
 
@@ -2397,7 +2381,6 @@ methods: {
 
     // if the event coming in is the keystroke after a '$' then check to change the type
     if (event && this.nextInputIsTypeSelection){
-      console.info("getting here?")
       if (event.data.toLowerCase()==='a' || event.data.toLowerCase()==='x'){
         this.typeLookup[this.activeComponentIndex] = 'madsrdf:Topic'
         this.subjectString=this.subjectString.replace('$'+event.data,'')
@@ -2441,7 +2424,6 @@ methods: {
       this.searchApis("", "", this)
     }
     if (!this.subjectString.endsWith("-")){
-      console.info("buildComponents")
       this.buildComponents(this.subjectString)
     }
 
@@ -2484,9 +2466,7 @@ methods: {
 
     }
 
-    console.info("updateAvctiveTypeSelected>>>>>>>>>>>>")
     this.updateAvctiveTypeSelected()
-    console.info("updateAvctiveTypeSelected<<<<<<<<<<<<")
 
     if (this.components.length==1 && this.components[0].complex){
       this.showTypes=false
@@ -2507,9 +2487,7 @@ methods: {
           if (this.localContextCache[x.uri]){
             if (this.activeComponent.type){
               // don't do anything
-              console.info("Type is already set")
             } else {
-              console.info("Type is not set")
               if (this.localContextCache[x.uri].nodeMap && this.localContextCache[x.uri].nodeMap['MADS Collection'] && this.localContextCache[x.uri].nodeMap['MADS Collection'].includes('GeographicSubdivisions')){
                 x.type = 'madsrdf:Geographic'
               }
