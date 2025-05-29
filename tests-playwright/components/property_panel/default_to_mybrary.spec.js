9b04f3289fc3f5a4500ac6e5fb46ed45042c17d6
f-osorio <fosorio@loc.gov>
2025-05-29 15:55:44 -0400

    start testing

--- tests-playwright/components/property_panel/default_to_mybrary.spec.js
+++ tests-playwright/components/property_panel/default_to_mybrary.spec.js
@@ -2,9 +2,28 @@
 // Tests that we are able to add a default component to my library
 import { test, expect } from '@playwright/test';
 
-test('AdminMetadata is present where appropriate.', async ({ page }) => {
-  await page.goto('http://localhost:5555/bfe2/quartz/');
+import * as jsonData from "@/../../tests-playwright/components/property_panel/marva_prefs_component_library.json"
 
-  await page.getByText('Click Here').click();
+//https://medium.com/@semihkasimoglu/leveraging-localstorage-and-sessionstorage-in-playwright-tests-with-typescript-949a4d0dee68
+
+test('Can add a default component to my component library.', async ({ page }) => {
+    await page.goto('http://localhost:5555/bfe2/quartz/');
+
+    let storage = await page.evaluate(() => window.localStorage);
+    // console.log("storage: ", storage)
+    await page.evaluate(setPreference)
+
+    await page.getByText('Click Here').click();
+    await page.getByRole('button', { name: 'Monograph', exact: true }).nth(1).click();
+    await page.locator('summary').filter({ hasText: 'library_add Defaults:' }).locator('span').nth(3).click();
+    await page.getByRole('listitem').filter({ hasText: 'At headadd' }).getByRole('button').click();
+    page.on('dialog', dialog => dialog.accept());
+    await expect(page.locator('#app')).toContainText('At head [D]');
+
 
 });
+
+function setPreference(){
+    console.log("data: ", jsonData)
+    localStorage.setItem("marva-preferences", JSON.stringify(jsonData))
+}
