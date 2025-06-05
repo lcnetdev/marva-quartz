beaa9cc6299ffbf7d82f063bd29823da77c2dbdf
f-osorio <fosorio@loc.gov>
2025-06-05 08:30:17 -0400

    Adjust form size when popping out a panel

--- src/components/panels/sidebar_preview_marc/WindowPortal.vue
+++ src/components/panels/sidebar_preview_marc/WindowPortal.vue
@@ -1,9 +1,8 @@
 <!-- https://stackoverflow.com/questions/49657462/open-a-vuejs-component-on-a-new-window -->
 <template>
-    <template v-if="open">
-        {{ styles }}
-         <MarcDisplay :previewData="content" selected="" v-if="type == 'marc'"/>
-    </template>
+        <template v-if="open">
+            <MarcDisplay :previewData="content" selected="" v-if="type == 'marc'"/>
+        </template>
 </template>
 
 <script>
@@ -38,11 +37,12 @@ export default {
     data() {
         return {
             windowRef: null,
+            currentPanels: null,
         }
     },
     computed: {
         ...mapStores(usePreferenceStore),
-        ...mapState(usePreferenceStore, ['styleDefault']),
+        ...mapState(usePreferenceStore, ['styleDefault', 'panelSizePresets']),
     },
     watch: {
         open(newOpen) {
@@ -59,6 +59,9 @@ export default {
             console.info('source: ', this.sourceDoc)
             console.info('styleSheets: ', this.sourceDoc.styleSheets)
             console.info("targetDoc: ", targetDoc)
+            console.info("this: ", window.document)
+
+            // Copy the style from the component we are duplicating
             Array.from(this.sourceDoc.styleSheets).forEach(styleSheet => {
                 if (styleSheet.cssRules){
                     const newStyleEl = this.sourceDoc.createElement('style')
@@ -67,11 +70,18 @@ export default {
                     })
 
                     targetDoc.head.appendChild(newStyleEl)
-                } else if (styleSheet.href){
-                    const newLinkEl = this.sourceDoc.createElement('link')
-                    newLinkEl.rel = 'stylesheet'
-                    newLinkEl.href = styleSheet.href
-                    targetDoc.head.appendChild(newLinkEl)
+                }
+            })
+
+            //Copy the style from there
+            Array.from(window.document.styleSheets).forEach(styleSheet => {
+                if (styleSheet.cssRules){
+                    const newStyleEl = window.document.createElement('style')
+                    Array.from(styleSheet.cssRules).forEach(cssRule => {
+                        newStyleEl.appendChild(window.document.createTextNode(cssRule.cssText))
+                    })
+
+                    targetDoc.head.appendChild(newStyleEl)
                 }
             })
         },
@@ -82,9 +92,54 @@ export default {
             // magic!
             this.windowRef.document.body.appendChild(this.$el);
             this.copyStyles(this.windowRef.document)
+
+            //adjust the panel sizes
+            //get the current info and store it
+            this.currentPanels = this.preferenceStore.getPanelData()
+
+            // adjust the sizes to hide the one being popped out
+            let newPanels = JSON.parse(JSON.stringify(this.currentPanels))
+            let previewPanelSizes = Object.keys(newPanels.percents).filter((panel) => !panel.includes('-edit-') && !panel.includes('properties'))
+            let previewEditSizes = Object.keys(newPanels.percents).filter((panel) => panel.includes('-edit-'))
+            let previewSize = 0
+            let nullPanels = []
+            let adjustedPanels = []
+
+            // get the size of the panel we're going to dup
+            for (let panel of previewPanelSizes){
+                if (panel.includes(this.type) && newPanels.percents[panel]){
+                    previewSize += Number(newPanels.percents[panel].slice(0,2))
+                }
+            }
+
+            // find the edit panels that have a size (in dual edit or not)
+            for (let panel of previewEditSizes){
+                if (newPanels.percents[panel]){
+                    adjustedPanels.push(panel)
+                } else {
+                    nullPanels.push(panel)
+                }
+            }
+
+            for (let key in newPanels.percents){
+                if (key.includes(this.type)){
+                    newPanels.percents[key] = "1%" // shrink the panel we are duping in a new window
+                } else if (adjustedPanels.includes(key)){ // add the space to the form panels
+                    let size = Number(newPanels.percents[key].slice(0,2))
+                    if (adjustedPanels.length == 1){
+                        newPanels.percents[key] = size + previewSize + "%"
+                    } else {
+                        newPanels.percents[key] = size + (previewSize/2) + "%"
+                    }
+                }
+            }
+
+            // move to adjusted panels
+            this.preferenceStore.setPanelData(newPanels)
         },
         closePortal() {
             console.info("closing")
+            this.preferenceStore.setPanelData(this.currentPanels)
             if (this.windowRef) {
                 this.windowRef.close();
                 this.windowRef = null;
@@ -96,6 +151,9 @@ export default {
         if (this.open) {
             this.openPortal();
         }
+
+        this.panelSizePresets = this.preferenceStore.returnValue('--o-edit-main-splitpane-edit-panel-size-presets')
+        console.info("panelSizePresets: ", this.panelSizePresets)
     },
     beforeDestroy() {
         console.info("destroy")
@@ -105,3 +163,12 @@ export default {
     }
 }
 </script>
+
+<!--  -->
+
+<style scoped>
+body {
+    background-color: v-bind("preferenceStore.returnValue('--c-edit-main-splitpane-opac-background-color')") !important;
+    color: v-bind("preferenceStore.returnValue('--c-edit-main-splitpane-opac-font-color')") !important;
+}
+</style>
