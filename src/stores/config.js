51f862594cd7cf263df50bdd9410821f29080640
f-osorio <fosorio@loc.gov>
2025-06-06 08:50:59 -0400

    Fix failing e2e test

--- src/stores/config.js
+++ src/stores/config.js
@@ -172,7 +172,6 @@ export const useConfigStore = defineStore('config', {
         starting: 'https://raw.githubusercontent.com/lcnetdev/bfe-profiles/main/starting-prod/data.json',
         id: 'https://id.loc.gov/',
         env : 'production',
-        publicEndpoints:true,
         displayLCOnlyFeatures: true,
         simpleLookupLang: 'en',
       },
