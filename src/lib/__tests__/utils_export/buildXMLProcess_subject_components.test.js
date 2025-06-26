97207c09a534efedc7e918303dc683ccb8055fec
f-osorio <fosorio@loc.gov>
2025-06-26 09:20:09 -0400

    More tests

--- src/lib/__tests__/utils_export/buildXMLProcess_subject_components.test.js
+++ src/lib/__tests__/utils_export/buildXMLProcess_subject_components.test.js
@@ -7,7 +7,7 @@ import { usePreferenceStore } from '@/stores/preference'
 import { createPinia } from 'pinia'
 import { createApp } from 'vue'
 
-import { monograph_subject_simple, monograph_subject_simple_simple_sub, monograph_subject_personal_name } from './xml/monograph_subject_simple.json'
+import { monograph_subject_simple, monograph_subject_simple_simple_sub, monograph_subject_personal_name, monograph_subject_geo, monograph_subject_hub, monograph_subject_complex } from './xml/monograph_subject_simple.json'
 
 const pinia = createPinia()
 const app = createApp()
@@ -21,14 +21,15 @@ usePreferenceStore().catInitals = "test"
 usePreferenceStore().catCode = "test"
 
 
-
-
 const expectedSimpleSubjectXml = `<bf:subject><madsrdf:Topic rdf:about="http://id.loc.gov/authorities/subjects/sh85038796"><madsrdf:isMemberOfMADSScheme rdf:resource="http://id.loc.gov/authorities/subjects"/><madsrdf:authoritativeLabel>Dogs</madsrdf:authoritativeLabel><rdfs:label>Dogs</rdfs:label><bflc:marcKey>150  $aDogs</bflc:marcKey><bf:source><bf:Source rdf:about="http://id.loc.gov/vocabulary/subjectSchemes/lcsh"><rdfs:label>Library of Congress subject headings</rdfs:label></bf:Source></bf:source></madsrdf:Topic></bf:subject>`
 const expectedSimpleSubjectSimpleSubXml = `<bf:subject><madsrdf:ComplexSubject><madsrdf:isMemberOfMADSScheme rdf:resource="http://id.loc.gov/authorities/subjects"/><madsrdf:authoritativeLabel>Dogs--History</madsrdf:authoritativeLabel><rdfs:label>Dogs--History</rdfs:label><madsrdf:componentList rdf:parseType="Collection"><madsrdf:Topic rdf:about="http://id.loc.gov/authorities/subjects/sh85038796"><madsrdf:authoritativeLabel>Dogs</madsrdf:authoritativeLabel><bflc:marcKey>150  $aDogs</bflc:marcKey></madsrdf:Topic><madsrdf:Topic rdf:about="http://id.loc.gov/authorities/subjects/sh99005024"><madsrdf:authoritativeLabel>History</madsrdf:authoritativeLabel><bflc:marcKey>180  $xHistory</bflc:marcKey></madsrdf:Topic></madsrdf:componentList><bf:source><bf:Source rdf:about="http://id.loc.gov/vocabulary/subjectSchemes/lcsh"><rdfs:label>Library of Congress subject headings</rdfs:label></bf:Source></bf:source></madsrdf:ComplexSubject></bf:subject>`
 const expectedPersonalNameXml = `<bf:subject><madsrdf:PersonalName rdf:about="http://id.loc.gov/authorities/names/n79021164"><madsrdf:isMemberOfMADSScheme rdf:resource="http://id.loc.gov/authorities/subjects"/><madsrdf:authoritativeLabel>Twain, Mark, 1835-1910</madsrdf:authoritativeLabel><rdfs:label>Twain, Mark, 1835-1910</rdfs:label><bflc:marcKey>1001 $aTwain, Mark,$d1835-1910</bflc:marcKey><bf:source><bf:Source rdf:about="http://id.loc.gov/vocabulary/subjectSchemes/lcsh"><rdfs:label>Library of Congress subject headings</rdfs:label></bf:Source></bf:source></madsrdf:PersonalName></bf:subject>`
-
+const expectedGeoXml = `<bf:subject><madsrdf:Geographic rdf:about="http://id.loc.gov/authorities/names/n79006971"><madsrdf:isMemberOfMADSScheme rdf:resource="http://id.loc.gov/authorities/subjects"/><madsrdf:authoritativeLabel>Spain</madsrdf:authoritativeLabel><rdfs:label>Spain</rdfs:label><bflc:marcKey>151  $aSpain</bflc:marcKey><bf:source><bf:Source rdf:about="http://id.loc.gov/vocabulary/subjectSchemes/lcsh"><rdfs:label>Library of Congress subject headings</rdfs:label></bf:Source></bf:source></madsrdf:Geographic></bf:subject>`
+const expectedHubXml = `<bf:subject><bf:Hub rdf:about="http://id.loc.gov/resources/hubs/7b8475be-4aeb-83dc-7bf7-18a0dc7eae58"><madsrdf:authoritativeLabel>Euripides. Medea</madsrdf:authoritativeLabel><rdfs:label>Euripides. Medea</rdfs:label><bflc:marcKey>1000 $aEuripides.$tMedea</bflc:marcKey></bf:Hub></bf:subject>`
+const expectedComplexXml = `<bf:subject><madsrdf:ComplexSubject><madsrdf:isMemberOfMADSScheme rdf:resource="http://id.loc.gov/authorities/subjects"/><madsrdf:authoritativeLabel>Food--Portugal--Porto--History--20th century</madsrdf:authoritativeLabel><rdfs:label>Food--Portugal--Porto--History--20th century</rdfs:label><madsrdf:componentList rdf:parseType="Collection"><madsrdf:Topic rdf:about="http://id.loc.gov/authorities/subjects/sh85050184"><madsrdf:authoritativeLabel>Food</madsrdf:authoritativeLabel><bflc:marcKey>150  $aFood</bflc:marcKey></madsrdf:Topic><madsrdf:HierarchicalGeographic rdf:about="http://id.loc.gov/authorities/names/n50006403-781"><madsrdf:authoritativeLabel>Portugal--Porto</madsrdf:authoritativeLabel><bflc:marcKey>181  $zPortugal$zPorto</bflc:marcKey></madsrdf:HierarchicalGeographic><madsrdf:Topic rdf:about="http://id.loc.gov/authorities/subjects/sh99005024"><madsrdf:authoritativeLabel>History</madsrdf:authoritativeLabel><bflc:marcKey>180  $xHistory</bflc:marcKey></madsrdf:Topic><madsrdf:Temporal rdf:about="http://id.loc.gov/authorities/subjects/sh2002012476"><madsrdf:authoritativeLabel>20th century</madsrdf:authoritativeLabel><bflc:marcKey>182  $y20th century</bflc:marcKey></madsrdf:Temporal></madsrdf:componentList><bf:source><bf:Source rdf:about="http://id.loc.gov/vocabulary/subjectSchemes/lcsh"><rdfs:label>Library of Congress subject headings</rdfs:label></bf:Source></bf:source></madsrdf:ComplexSubject></bf:subject>`
 
 describe('Subject XML is correct', () => {
+    // Dogs
     describe("Simple Subject", () => {
         test('should contain the expectedSimpleSubjectXml', async () => {
             let profile = monograph_subject_simple
@@ -40,6 +41,7 @@ describe('Subject XML is correct', () => {
         });
     })
 
+    // Dogs--History
     describe("Simple Subject with simple subdivision", () => {
         test('should contain the expectedSimpleSubjectSimpleSubXml', async () => {
             let profile = monograph_subject_simple_simple_sub
@@ -51,6 +53,7 @@ describe('Subject XML is correct', () => {
         });
     })
 
+    // Twain, mark
     describe("Personal name", () => {
         test('should contain the expectedPersonalNameXml', async () => {
             let profile = monograph_subject_personal_name
@@ -61,9 +64,41 @@ describe('Subject XML is correct', () => {
         });
     })
 
-    // Geographic
+    // Geographic - Spain
+    describe("Geogephic Subject", () => {
+        test('should contain the expectedGeoXml', async () => {
+            let profile = monograph_subject_geo
+            useProfileStore().activeProfile = profile
+            let xmlList = await utils_export.buildXMLProcess(profile)
+            let xmlString = xmlList.xlmStringBasic
+            expect(xmlString).toContain(expectedGeoXml)
+        });
+    })
+
+    // Hub - Euripides. Medea
+    describe("Hub Subject", () => {
+        test('should contain the expectedHubXml', async () => {
+            let profile = monograph_subject_hub
+            useProfileStore().activeProfile = profile
+            let xmlList = await utils_export.buildXMLProcess(profile)
+            let xmlString = xmlList.xlmStringBasic
+            expect(xmlString).toContain(expectedHubXml)
+        });
+    })
+
+    // Complex topic Food--Portugal--Porto--History--20th century
+    describe("Complex Subject", () => {
+        test('should contain the expectedHubXml', async () => {
+            let profile = monograph_subject_complex
+            useProfileStore().activeProfile = profile
+            let xmlList = await utils_export.buildXMLProcess(profile)
+            let xmlString = xmlList.xlmStringBasic
+            expect(xmlString).toContain(expectedComplexXml)
+        });
+    })
+
 
     // person subdivision
 
-    // Hub
+
 });
