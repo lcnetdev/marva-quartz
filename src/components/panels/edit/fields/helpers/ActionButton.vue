4536d30a3e2257825078b52ee0765d8f439f566e
f-osorio <fosorio@loc.gov>
2025-07-08 15:32:21 -0400

    Clean console

--- src/components/panels/edit/fields/helpers/ActionButton.vue
+++ src/components/panels/edit/fields/helpers/ActionButton.vue
@@ -354,7 +354,6 @@
       buildHubStub(){
         // console.log(this.guid)
         let info = this.profileStore.returnLccInfo(this.guid)
-        console.info("info: ", info)
         this.profileStore.activeHubStubData = info
         this.profileStore.activeHubStubComponent = {
           type: this.type,
