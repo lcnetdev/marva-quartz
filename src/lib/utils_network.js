08fa12661f45ae8ac37af59adc02d42d8fffa921
f-osorio <fosorio@loc.gov>
2025-08-06 11:47:03 -0400

    Remove `nocache` from shelflist search
    Caused order issue in the results for some reason.

--- src/lib/utils_network.js
+++ src/lib/utils_network.js
@@ -3254,7 +3254,7 @@ const utilsNetwork = {
         dir ='ascending'
       }
 
-      let urlSearch = "lds/browse.xqy?bq=" + search.toUpperCase() +"&browse-order=" + dir + "&browse=class" + details + "&mime=json"  + '?nocache='+Date.now()
+      let urlSearch = "lds/browse.xqy?bq=" + search.toUpperCase() +"&browse-order=" + dir + "&browse=class" + details + "&mime=json"
 
       // try{
         //let req = await fetch(useConfigStore().returnUrls.shelfListing + `browse/class/${dir}/${search}.json` )
