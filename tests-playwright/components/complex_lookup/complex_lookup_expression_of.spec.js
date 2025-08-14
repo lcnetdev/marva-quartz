7c64e2677268ac3863aee4db3f5a9cf646e444a0
f-osorio <fosorio@loc.gov>
2025-08-14 14:42:36 -0400

    Fix failing e2e test

--- tests-playwright/components/complex_lookup/complex_lookup_expression_of.spec.js
+++ tests-playwright/components/complex_lookup/complex_lookup_expression_of.spec.js
@@ -3,16 +3,16 @@ import { test, expect } from '@playwright/test';
 
 test('Add an expression Of to monograph record', async ({ page }) => {
   await page.goto('http://localhost:5555/bfe2/quartz/');
-  
+
   await page.getByText('Click Here').click();
   await page.getByRole('button', { name: 'Monograph', exact: true }).nth(1).click();
   await page.locator('[id="edit_lc\\:RT\\:bf2\\:Monograph\\:Work_id_loc_gov_ontologies_bibframe_expressionOf__expression_of_\\[search_for_a_hub\\]"]').getByRole('textbox').click();
   await page.locator('[id="edit_lc\\:RT\\:bf2\\:Monograph\\:Work_id_loc_gov_ontologies_bibframe_expressionOf__expression_of_\\[search_for_a_hub\\]"]').getByRole('textbox').fill('Woolf, Virginia, 1882-1941. To the lighthouse');
   await expect(page.getByRole('listbox')).toBeVisible();
   await expect(page.getByRole('listbox')).toContainText('Woolf, Virginia, 1882-1941. To the lighthouse (Auth) [RDA]');
- 
-  await page.getByRole('dialog').getByRole('textbox').click();
-  await page.getByRole('dialog').getByRole('textbox').press('ArrowDown');
+
+  await page.getByRole('dialog').getByRole('listbox').click();
+  await page.getByRole('dialog').getByRole('listbox').press('ArrowDown');
   await expect(page.getByRole('listbox')).toContainText('Woolf, Virginia, 1882-1941. To the lighthouse (Auth) [RDA]');
   await expect(page.getByRole('dialog')).toContainText('Identifiers: no2017122683 ; oca10985804');
   await page.locator('summary').filter({ hasText: 'Extra Details' }).click();
