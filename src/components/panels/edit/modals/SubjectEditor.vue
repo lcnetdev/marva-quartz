cebb7af96ccfbe1b42a48d1963bdda3df864a4ad
fo <fosorio@loc.gov>
2025-09-16 14:56:37 +0000

    Better stitching loose components with subdivision
    If a headings has a Indirect GEO and a subdivision, the switch to
    "Indirect GEO" would add the subdivision as part of the indirect geo.
    This should prevent that.

--- src/components/panels/edit/modals/SubjectEditor.vue
+++ src/components/panels/edit/modals/SubjectEditor.vue
@@ -1284,6 +1284,7 @@ export default {
        */
 
       if (mode == "GEO") {
+        console.info("components 1: ", JSON.parse(JSON.stringify(this.components)))
         // if the User selected the first part of an indirect geo from the LCSH/LCNAF
         // search and then swaps to GEO to finish, replace the `--` between the two
         // to ease the process
@@ -1301,18 +1302,19 @@ export default {
             this.navStringClick({})
           }
         }
-
-
+        // art schools--massachusetts--boston--biography
         /**
          * When dealing with a switch to GEO, we need to combine the "loose" components
          * into 1 so the search will work.
          */
+
+        console.info("components 2: ", JSON.parse(JSON.stringify(this.components)))
         //get the loose components
         let looseComponents = []
         let indx = []
         let componentMap = []
         for (let c in this.components) {
-          if (this.components[c].uri == null && this.components[c].literal != true) {
+          if (this.components[c].uri == null && this.components[c].literal != true && !this.components[c].label.includes("‑‑")) {
             looseComponents.push(this.components[c])
             indx.push(c)
             componentMap.push("-")
@@ -1320,6 +1322,7 @@ export default {
             componentMap.push(c)
           }
         }
+        console.info("looseComponents: ", JSON.parse(JSON.stringify(looseComponents)))
         //only stitch the loose components togethere if there are 2 next to each other
         if (indx.length == 2 && indx[1] - 1 == indx[0]) {
           /** !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
@@ -1343,9 +1346,12 @@ export default {
               } else {
                 part1 = this.activeComponent.label
               }
-              const part2 = looseComponents[c].label
-              this.activeComponent.label = part1 + "‑‑" + part2
-              this.activeComponent.posEnd = looseComponents[c].posEnd
+              console.info("part1: ", part1)
+              if (!part1.includes("‑‑")){
+                const part2 = looseComponents[c].label
+                this.activeComponent.label = part1 + "‑‑" + part2
+                this.activeComponent.posEnd = looseComponents[c].posEnd
+              }
             }
           }
           this.activeComponent.posStart = looseComponents[0].posStart
