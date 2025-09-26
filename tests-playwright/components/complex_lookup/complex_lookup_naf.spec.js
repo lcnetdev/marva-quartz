a9077885dde8db045e9396c34b1b25fcfe74aedf
fo <fosorio@loc.gov>
2025-09-26 13:58:28 +0000

    adjust test

--- tests-playwright/components/complex_lookup/complex_lookup_naf.spec.js
+++ tests-playwright/components/complex_lookup/complex_lookup_naf.spec.js
@@ -8,11 +8,11 @@ test('Basic NAF Search and Add Monograph Profile', async ({ page }) => {
   await page.getByRole('button', { name: 'Monograph', exact: true }).nth(1).click();
   await page.locator('[id="edit_lc\\:RT\\:bf2\\:Monograph\\:Work_id_loc_gov_ontologies_bibframe_contribution__creator_of_work"] div').filter({ hasText: 'Select' }).getByRole('textbox').click();
   await page.locator('form').filter({ hasText: 'Search LCNAFbolt' }).getByRole('textbox').fill('s');
-  await page.getByRole('dialog').getByRole('textbox').fill('smith');
-  await page.getByRole('listbox').selectOption('http://id.loc.gov/authorities/names/nb2013011585');
+  await page.getByRole('dialog').getByRole('textbox').fill('smith, alan');
+  await page.getByRole('listbox').selectOption('http://id.loc.gov/authorities/names/n94104979');
   await expect(page.getByRole('dialog')).toContainText('Smith, Alan, 1931-');
   await page.getByRole('button', { name: 'Add [Shift+Enter]' }).click();
-  await expect(page.locator('[id="edit_lc\\:RT\\:bf2\\:Monograph\\:Work_id_loc_gov_ontologies_bibframe_contribution__creator_of_work"]')).toContainText('Smith, A. (Alan), 1931-');
+  await expect(page.locator('[id="edit_lc\\:RT\\:bf2\\:Monograph\\:Work_id_loc_gov_ontologies_bibframe_contribution__creator_of_work"]')).toContainText('Smith, Alan, 1933-');
 
 
 });
