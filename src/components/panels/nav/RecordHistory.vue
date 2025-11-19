26e7381cef2c8897946a5b033b0e53f67e1ed025
fo <fosorio@loc.gov>
2025-11-19 16:59:12 +0000

    Add more details from adminmetadata

--- src/components/panels/nav/RecordHistory.vue
+++ src/components/panels/nav/RecordHistory.vue
@@ -1,9 +1,13 @@
 <template>
-    <div>
+    <div class="container">
         <div class="record-id">
             ID: <a :href="'https://id.loc.gov/resources/works/' + recordId" target="_blank">{{ recordId }}</a>
+            <br>
+            Encoding Level: {{ encLvl }}
+            <br>
+            Cataloging Authentication Code(s): {{ authentication.join(" ") }}
         </div>
-        <p v-if="adminMetadata.length > 0">History</p>
+        <p>History</p>
         <table class="history">
             <tr v-for="datum in adminMetadata">
                 <td>
@@ -32,6 +36,8 @@ export default {
             recordId: null,
             adminMetadata: null,
             nineXX: null,
+            encLvl: null,
+            authentication: null
         }
     },
     props: {
@@ -63,16 +69,29 @@ export default {
                         let dt = new Date(date).toLocaleString()
                         admin.push({ "date": date, "status": status, "dateString": dt })
                     } catch {
+                        console.info("data: ", data)
                         try {
                             for (let thing in data) {
                                 if (thing.includes("/d9")) {
                                     nines.push(data[thing][0][thing])
                                 }
                             }
-                        } catch { }
-                        try {
+                        } catch {}
+                        if (Object.keys(data).includes("http://id.loc.gov/ontologies/bibframe/identifiedBy")){
                             this.recordId = data["http://id.loc.gov/ontologies/bibframe/identifiedBy"][0]["http://www.w3.org/1999/02/22-rdf-syntax-ns#value"][0]["http://www.w3.org/1999/02/22-rdf-syntax-ns#value"]
-                        }catch { }
+                        }
+                        if (Object.keys(data).includes("http://id.loc.gov/ontologies/bflc/encodingLevel")){
+                            this.encLvl = data["http://id.loc.gov/ontologies/bflc/encodingLevel"][0]["http://www.w3.org/2000/01/rdf-schema#label"][0]["http://www.w3.org/2000/01/rdf-schema#label"]
+                        }
+                        if (Object.keys(data).includes("http://id.loc.gov/ontologies/bibframe/descriptionAuthentication")){
+                            let authDesc = []
+                            for (let auth of data["http://id.loc.gov/ontologies/bibframe/descriptionAuthentication"]){
+                                let id = auth["@id"].split("/").at(-1)
+                                authDesc.push(id)
+                            }
+                            this.authentication = authDesc
+                        }
+
                     }
                 }
             }
@@ -93,10 +112,12 @@ export default {
 </script>
 
 <style scoped>
+.container {
+    padding: 5px;
+}
 .history {
     border: 1px solid black;
 }
-
 
 tr:hover {
     background-color: rgb(143, 141, 141);
@@ -105,5 +126,9 @@ tr:hover {
 
 .nines {
     margin-top: 8px;
+}
+
+.record-id {
+    margin-bottom: 8px;
 }
 </style>
