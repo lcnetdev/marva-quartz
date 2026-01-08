db5045ad73c59e1f56bac8250efdece70201769d
fo <fosorio@loc.gov>
2026-01-08 20:35:41 +0000

    Update Marva Manual URL

--- src/components/panels/nav/Nav.vue
+++ src/components/panels/nav/Nav.vue
@@ -208,7 +208,7 @@ export default {
           {
             text: 'LC Marva Manual',
             click: () => {
-              const routeData = window.open('https://libgov-my.sharepoint.com/personal/pfrank_lib_loc_gov/_layouts/15/onedrive.aspx?id=%2Fpersonal%2Fpfrank%5Flib%5Floc%5Fgov%2FDocuments%2FMarva%2DManual%2DShare%2FLibrary%2Dof%2DCongress%2DMarva%2DQuartz%2DUser%2DManual%2Epdf&parent=%2Fpersonal%2Fpfrank%5Flib%5Floc%5Fgov%2FDocuments%2FMarva%2DManual%2DShare')
+              const routeData = window.open('https://www.loc.gov/catworkshop/bibframe/Library-of-Congress-Marva-Quartz-User-Manual.pdf')
             },
             icon: "📄"
           }
