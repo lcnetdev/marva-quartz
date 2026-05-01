bdc83bb325a4ecdf89b4a46a7ea1627024679b2e
fo <fosorio@loc.gov>
2026-05-01 12:42:05 +0000

    Cleanup

--- src/components/panels/edit/modals/NacoStubCreateModal.vue
+++ src/components/panels/edit/modals/NacoStubCreateModal.vue
@@ -1861,7 +1861,6 @@
                   </div>
                 </div>
               </div>
-              ??{{ hyphenated4xx }}!!{{ buildHyphenated4xx }}??
               <div v-if="hyphenated4xx && !preferenceStore.returnValue('--b-edit-complex-nar-advanced-mode')" style="margin-bottom: 0.75em;">
                 <input type="checkbox" id="buildHyphenated4xx" name="buildHyphenated4xx" v-model="buildHyphenated4xx" style="margin-right: 1em;"/>
                 <label for="buildHyphenated4xx" style="vertical-align: super;">Add Hyphenated 4XX: <span style="background-color: whitesmoke; font-family: 'Courier New', Courier, monospace;">{{ hyphenated4xx.preview }}</span></label>
